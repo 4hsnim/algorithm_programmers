890479a97f5998988efcde7ba8b7e64e44921dc6
4hsnim <4hsnim@naver.com>
2022-08-15 20:56:04 +0900

    콜라츠 추측 풀이 완료 후 재커밋

--- 1단계/2022-08-15_1.js
+++ 1단계/2022-08-15_1.js
@@ -36,10 +36,8 @@ function solution(num) {
       return i;
     } else if (num % 2 === 0) {
       num = num / 2;
-      i++;
     } else if (num % 2 === 1 && num !== 1) {
       num = num * 3 + 1;
-      i++;
     }
   }
 }
