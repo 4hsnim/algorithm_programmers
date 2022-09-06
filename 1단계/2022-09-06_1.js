48d251767790549b8737095e8bcb904c21436a2b
4hsnim <4hsnim@naver.com>
2022-09-06 17:26:22 +0900

    체육복 풀이완료

---
+++ 1단계/2022-09-06_1.js
@@ -0,0 +1,53 @@
+// 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/42862
+// 체육복
+// 문제 설명
+// 점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다. 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다. 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+// 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+
+// 제한사항
+// 전체 학생의 수는 2명 이상 30명 이하입니다.
+// 체육복을 도난당한 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌의 체육복을 가져온 학생의 수는 1명 이상 n명 이하이고 중복되는 번호는 없습니다.
+// 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
+// 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+// 입출력 예
+// n	lost	reserve	return
+// 5	[2, 4]	[1, 3, 5]	5
+// 5	[2, 4]	[3]	4
+// 3	[3]	[1]	2
+// 입출력 예 설명
+// 예제 #1
+// 1번 학생이 2번 학생에게 체육복을 빌려주고, 3번 학생이나 5번 학생이 4번 학생에게 체육복을 빌려주면 학생 5명이 체육수업을 들을 수 있습니다.
+
+// 예제 #2
+// 3번 학생이 2번 학생이나 4번 학생에게 체육복을 빌려주면 학생 4명이 체육수업을 들을 수 있습니다.
+
+// 풀이
+function solution(n, lost, reserve) {
+  var answer = n - lost.length;
+  // 처음 가능한 학생수 = n - lost.length
+  // lost 배열 앞뒤의 값을 reserve에 포함되어있는지를 확인 -> 해당값을 reserve에서 뺌 + answer++
+  // 왜 정렬을 해줘야 하지? - [4,2], [3,5]와 같은 케이스 때문
+
+  let realLost = lost.filter((l) => !reserve.includes(l));
+  let realReserve = reserve.filter((r) => !lost.includes(r));
+  answer += lost.length - realLost.length;
+
+  realLost.sort((a, b) => a - b);
+
+  realLost.forEach((l) => {
+    if (realReserve.length === 0) {
+      return;
+    }
+
+    if (realReserve.includes(l - 1)) {
+      realReserve = realReserve.filter((r) => r !== l - 1);
+      answer++;
+    } else if (realReserve.includes(l + 1)) {
+      realReserve = realReserve.filter((r) => r !== l + 1);
+      answer++;
+    }
+  });
+  return answer;
+}
