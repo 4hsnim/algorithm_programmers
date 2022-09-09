75d177d6669a64194412af0b9cee0b2732193f63
4hsnim <4hsnim@naver.com>
2022-09-09 10:34:27 +0900

    K번째 수 풀이완료

---
+++ 1단계/2022-09-09_1.js
@@ -0,0 +1,48 @@
+// 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/42748
+// K번째수
+// 문제 설명
+// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+// 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+// array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+// 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+// 2에서 나온 배열의 3번째 숫자는 5입니다.
+// 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+// 제한사항
+// array의 길이는 1 이상 100 이하입니다.
+// array의 각 원소는 1 이상 100 이하입니다.
+// commands의 길이는 1 이상 50 이하입니다.
+// commands의 각 원소는 길이가 3입니다.
+// 입출력 예
+// array	commands	return
+// [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+// 입출력 예 설명
+// [1, 5, 2, 6, 3, 7, 4]를 2번째부터 5번째까지 자른 후 정렬합니다. [2, 3, 5, 6]의 세 번째 숫자는 5입니다.
+// [1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. [6]의 첫 번째 숫자는 6입니다.
+// [1, 5, 2, 6, 3, 7, 4]를 1번째부터 7번째까지 자릅니다. [1, 2, 3, 4, 5, 6, 7]의 세 번째 숫자는 3입니다.
+
+// 내 풀이
+// Array.slice(startIndex,endIndex)
+// 만약 Array.slice(3,5)
+// index 3,4번째 배열의 값만 잘라서 새로운 배열로 만든다.
+// end : 추출을 종료 할 0 기준 인덱스입니다.
+// slice 는 end 인덱스를 제외하고 추출합니다. 예를 들어, slice(1,4)는 두번째 요소부터 네번째 요소까지 (1, 2 및 3을 인덱스로 하는 요소) 추출합니다.
+function solution(array, commands) {
+  var answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let slice = array.slice(commands[i][0] - 1, commands[i][1]);
+    let sort = slice.sort((a, b) => {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+    });
+    let num = sort[commands[i][2] - 1];
+    answer.push(num);
+  }
+  return answer;
+}
